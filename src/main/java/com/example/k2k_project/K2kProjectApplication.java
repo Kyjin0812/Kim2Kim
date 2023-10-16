61f441dea6d6d0db4fd7a248ee176424fa8ee062
Kim Yongjin <zkdlwls0812@gmail.com>
2023-10-16 23:53:08 +0900

    first commit

---
+++ src/main/java/com/example/k2k_project/K2kProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.k2k_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K2kProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K2kProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/k2k_project/K2kProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k2k_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K2kProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
